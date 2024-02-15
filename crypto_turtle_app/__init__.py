7c1761bfbd37fe6d9a90bac496011a0b6bbcca25
Matthew <matthew@thespainreport.com>
2024-02-15 14:24:08 +0100

    Non-www to www redirects site wide

--- crypto_turtle_app/__init__.py
+++ crypto_turtle_app/__init__.py
@@ -1,6 +1,34 @@
 import flask as fk
+import dotenv as de
+import os as os
 
+de.load_dotenv('.flaskenv')
 app = fk.Flask(__name__)
+
+print(f"FLASK_ENV 1: {os.getenv('FLASK_ENV')}")
+
+@app.before_request
+def enforce_www_and_https():    
+    url = fk.request.url
+    redirect_url = None
+    
+    print(f"FLASK_ENV 2: {os.getenv('FLASK_ENV')}")
+    
+    if os.getenv('FLASK_ENV') == 'development':
+        print("Skipping redirection for development environment")
+        return
+    
+    if not url.startswith('https://www.'):
+        if url.startswith('http://'):
+            redirect_url = url.replace('http://', 'https://www.', 1)
+        elif url.startswith('https://'):
+            redirect_url = url.replace('https://', 'https://www.', 1)
+        else:
+            redirect_url = 'https://www.' + url
+
+        code = 301
+        return fk.redirect(redirect_url, code=code)
+
 
 @app.route('/')
 def hello_world():
