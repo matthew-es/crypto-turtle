b95577dd18cf08e3e09920161f99319d6dae20e4
Matthew <matthew@thespainreport.com>
2024-02-15 13:21:55 +0100

    Deletes root level .py, puts routes in __init__ for local and production

--- crypto_turtle_app/__init__.py
+++ crypto_turtle_app/__init__.py
@@ -2,6 +2,19 @@ import flask as fk
 
 app = fk.Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    # return "Hello, World!"
+    return fk.render_template('home.html')
+
+@app.route('/btc-usd')
+def btc_usd():
+    return "BTC-USD"
+
+@app.route('/eth-usd')
+def eth_usd():
+    return "ETH-USD"
+
 # from flask import Flask
 # import config as cf
 
