1e34cce72b99c692b6dd0b6111a1c8d53e2db633
Matthew <matthew@thespainreport.com>
2024-02-16 18:42:16 +0100

    Adds sslmode require to db connection for Azure

--- crypto_turtle_program/crypto_turtle_database.py
+++ crypto_turtle_program/crypto_turtle_database.py
@@ -12,7 +12,8 @@ def db_connect_open():
             dbname = os.getenv("DATABASE_NAME"),
             user = os.getenv("DATABASE_USER"),
             password = os.getenv("DATABASE_PASSWORD"),
-            port = os.getenv("DATABASE_PORT")
+            port = os.getenv("DATABASE_PORT"),
+            sslmode="require"
         )
         return connection
         
