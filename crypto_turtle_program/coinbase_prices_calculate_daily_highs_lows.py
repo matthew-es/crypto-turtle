84a9cc3b338edbd58c731a964baf583ba807d98b
Matthew <matthew@thespainreport.com>
2024-02-17 20:43:06 +0100

    Now let's debug price high low checks

--- crypto_turtle_program/coinbase_prices_calculate_daily_highs_lows.py
+++ crypto_turtle_program/coinbase_prices_calculate_daily_highs_lows.py
@@ -20,7 +20,8 @@ def update_high_lows(symbols, new_connection):
                 print(f"Symbol {symbol} not found in Symbols table.")
                 continue
             symbol_id = symbol_id[0]
-            
+            log.log_message(f"CHECKING HIGH LOWS FOR: {symbol}")
+
             # Fetch UnixTimestamps that need updating
             cursor.execute("""
                 SELECT UnixTimestamp FROM DailyPrices
@@ -53,6 +54,8 @@ def update_high_lows(symbols, new_connection):
                         """
                         cursor.execute(update_query, (period_high, period_low, symbol_id, unixtimestamp))
                         new_connection.commit()
+            
+            log.log_message(f"CHECKING HIGH LOWS FOR: {symbol} DONE")
 
         except Exception as e:
             print(f"Error updating high and low for symbol {symbol}: {e}")
